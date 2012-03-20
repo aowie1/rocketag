92aebffeff228945bf69908c322db82f144038ef
Heath Naylor <Heathnaylor@gmail.com>
2012-03-19 23:41:25 -0600

    HT Access

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://rocketag.com';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
