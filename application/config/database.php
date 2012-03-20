14a07c51050e632ed910a60c752096788ac42186
Randen Kelly <aowie1@gmail.com>
2012-03-20 01:03:31 -0600

    updated DB creds for local env., ignoring email config to avoid posting sensitive login creds publicly

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '5M3y9H66';
+$db['default']['password'] = 'password';
 $db['default']['database'] = 'rocketag';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
