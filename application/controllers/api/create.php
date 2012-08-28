4cfefe40c902efac27e96dbb0b7d85b8d6326eed
Randen Kelly <aowie1@gmail.com>
2012-08-27 23:26:12 -0600

    more dev on create method

--- application/controllers/api/create.php
+++ application/controllers/api/create.php
@@ -17,17 +17,38 @@ class create extends CI_Controller {
     {
         $this->form_validation->set_rules('thing_name', 'Thing Name', 'required|max_length[75]|callback__valid_string|is_unique[things.name]');
         $this->form_validation->set_rules('tags[]', 'Tag', 'is_natural_no_zero|max_length[20]');
+        $this->form_validation->set_rules('anonymous', 'Anonymous Status', 'integer');
+        $this->form_validation->set_rules('category', 'Category', 'required|integer');
         
         if ($this->form_validation->run()) {
             $thing = $this->input->post('thing_name');
+            $anonymous = !empty($this->input->post('anonymous'));
+            $category_id = $this->input->post('category');
             
             $new_thing_id = $this->mapi->create_thing($thing);
             
             //Attempt to assign the tags if they were posted and the thing was successfully created
             if (!empty($_POST['tags']) && $new_thing_id) {
                 $tags = $this->input->post('tags');
-                $tag_assignment = $this->mapi->assign_tag($new_thing_id, $tags); 
-            }  
+                foreach ($tags as $tag_id) {
+                    $insert_tag_data[] = array(
+                        'things_id'      => $new_thing_id,
+                        'tags_id'        => $tag_id,
+                        'originator'    => (int) $this->mapi->check_thing_tag_join_exists($new_thing_id, $tag_id),
+                        'anonymous'     => (int) $anonymous,
+                        'created_ts'    => time()
+                    );
+                }
+                $tag_assignment = $this->mapi->relate_thing_tag($insert_tag_data);
+            }
+            
+            if (!empty($category_id)) {
+                $insert_category_data = array(
+                    'things_id'             => $new_thing_id,
+                    'thing_categories_id'   => $category_id
+                );
+                $this->mapi->relate_thing_category($insert_category_data);
+            }
             
             $status = !empty($new_thing_id);
         }else{
